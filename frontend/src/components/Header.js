c34759cc9eeb0e8ee2904e905267a644040086d3
Noor Mohammad <noormohammad.net1503011@gmail.com>
2022-12-18 04:06:14 +0600

    changing upload image

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -19,7 +19,7 @@ const Header = () => {
     <header>
       <Navbar bg='dark' variant='dark' expand='lg'>
         <Container>
-          <Navbar.Brand href='/'>ShopIt</Navbar.Brand>
+          <Navbar.Brand href='/'>Shop IT</Navbar.Brand>
           <Navbar.Toggle aria-controls='basic-navbar-nav' />
           <Navbar.Collapse id='basic-navbar-nav'>
             <Route render={({ history }) => <SearchBox history={history} />} />
